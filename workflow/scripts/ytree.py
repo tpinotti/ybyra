0f17f7f914ea62f6e9522228a044b44ddabcf44d
Thomaz Pinotti <54654667+tpinotti@users.noreply.github.com>
2025-12-15 20:13:54 +0100

    Update ytree.py
    major update on damage assessment module of ybyra (v.05).
    Now users can choose between three different models of damage to filter out damage.

--- workflow/scripts/ytree.py
+++ workflow/scripts/ytree.py
@@ -12,20 +12,49 @@ def depth_filter(row):
 
 #
 #	Damage filter
-#	Check if damage depending on lib type
+#	Check if damage depending on lib type and damage model
 #
 
-def assess_damage(row, lib_type):
+def assess_damage(row, lib_type, dmg_model):
     x, y, w, z = map(int, row['dp4'].split(','))
+    mut = row['mutation']
+    state = row['state']
 
-    if row['mutation'] == 'C>T':
-        return 'yes'
+    if lib_type in ['ds', 'both']:
+        right = ['C>T', 'G>A']
+        left = ['T>C', 'A>G']
+    else:	# 'ss'
+        right = ['C>T']
+        left = ['T>C']
 
-    elif row['mutation'] == 'G>A':
-        if lib_type == 'ss':
-            return 'no'
-        elif lib_type in ['ds', 'both']:
+    #
+    #	naive mode – simply remove all C>T (or G>A)
+    #
+    
+    if dmg_model == 'naive':
+        if mut in right:
             return 'yes'
+        return 'no'
+
+    #
+    #	unidirectional mode – remove all derived C>T (or G>A); keep all ancestral
+    #
+    	
+    elif dmg_model == 'uni':
+        if mut in right and state == 'derived':
+            return 'yes'
+        return 'no'
+
+    #
+    #	bidirectional mode – remove all derived C>T (or G>A); remove all ancestral T>C (or A>G)
+    #
+    
+    elif dmg_model == 'bi':
+        if mut in right and state == 'derived':
+            return 'yes'
+        if mut in left and state == 'ancestral':
+            return 'yes'
+        return 'no'
 
     return 'no'
 
@@ -33,7 +62,7 @@ def assess_damage(row, lib_type):
 #	Fiat lux
 #
 
-def main(lib_type, out_prefix, alleles_file, snpinfo_file):
+def main(lib_type, out_prefix, alleles_file, snpinfo_file, dmg_model):
 
     snpinfo = pd.read_csv(snpinfo_file, sep='\t')
     alleles = pd.read_csv(alleles_file, sep='\t', names=['position', 'ref', 'alt', 'coverage', 'qual', 'dp4', 'geno'])
@@ -54,11 +83,11 @@ def main(lib_type, out_prefix, alleles_file, snpinfo_file):
 
     nopass.to_csv(f'{out_prefix}.nopass', sep='\t', index=False)
 
-	# damage filter
+	# damage assesment
     if not hits.empty:
-         hits['damage'] = hits.apply(assess_damage, axis=1, lib_type=lib_type)
+        hits['damage'] = hits.apply(assess_damage, axis=1, lib_type=lib_type, dmg_model=dmg_model)
     else:
-         hits['damage'] = []
+        hits['damage'] = []
 
     hits.to_csv(f'{out_prefix}.calls', sep='\t', index=False)
 
@@ -66,9 +95,11 @@ def main(lib_type, out_prefix, alleles_file, snpinfo_file):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Match to tree and filter Y-chromosome genotypes")
     parser.add_argument('--lib', choices=['ds', 'ss', 'both'], required=True, help="Library build: 'ds' for double-stranded, 'ss' for single-stranded, 'both' if both were merged in bam")
+    parser.add_argument('--dmg', choices=['naive','uni','bi'], required=True, help="Damage model to assess damage: 'naive' to remove all C>T (and G>A depending on --lib), 'uni' for unidirectional damage model (only derived), 'bi' for bidirectional damage (both derived and ancestral)")
     parser.add_argument('--out', required=True, help="Output file prefix")
     parser.add_argument('--alleles', required=True, help="Path to the alleles file")
     parser.add_argument('--snpinfo', required=True, help="Path to the SNP info file")
     args = parser.parse_args()
 
-    main(args.lib, args.out, args.alleles, args.snpinfo)
+    main(args.lib, args.out, args.alleles, args.snpinfo, args.dmg)
+    
